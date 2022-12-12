ca133e0c89e7fb89bd0efad9b7f3a3a6b723e041
YMingF <834319313@qq.com>
2022-12-12 17:37:40 +0800

    4-10 滑块7，完成播放器缓冲条的效果

--- src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
+++ src/app/share/wy-ui/wy-slider/wy-slider-track.component.ts
@@ -11,12 +11,13 @@ import {WySliderStyle} from './wy-slider-types';
 @Component({
   selector: 'app-wy-slider-track',
   template: `
-    <div class="wy-slider-track" [ngStyle]="style"></div>`,
+    <div class="wy-slider-track" [class.buffer]="wyBuffer" [ngStyle]="style"></div>`,
   changeDetection:ChangeDetectionStrategy.OnPush // 在Input属性变化的时候才更新
 })
 export class WySliderTrackComponent implements OnInit, OnChanges {
   @Input() wyVertical = false;// 当前是否为垂直模式的进度条
   @Input() wyLength: number;// 当前进度条的长度
+  @Input() wyBuffer= false;
   style: WySliderStyle = {};
 
   constructor() {
