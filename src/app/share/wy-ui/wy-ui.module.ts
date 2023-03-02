cdfa10ee767f9e9ab674a2c76e564b8b2ef8febb
alex <alex@nanoinsure.com>
2023-03-02 20:31:23 +0800

    7-1-搜索组件一，

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from '@angular/core';
 import {SingleSheetComponent} from './single-sheet/single-sheet.component';
 import {PlayCountPipe} from '../pipes/play-count.pipe';
 import {WyPlayerModule} from './wy-player/wy-player.module';
+import { WySearchModule } from "./wy-search/wy-search.module";
 
 @NgModule({
   declarations: [
     SingleSheetComponent,
     PlayCountPipe
   ],
-  imports: [WyPlayerModule],
-  exports: [SingleSheetComponent, PlayCountPipe, WyPlayerModule]
+  imports: [WyPlayerModule,WySearchModule],
+  exports: [SingleSheetComponent, PlayCountPipe, WyPlayerModule,WySearchModule]
 })
 export class WyUiModule {}
