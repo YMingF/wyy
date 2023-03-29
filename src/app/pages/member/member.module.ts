7d14782fee8e76273d72c7614c5251c55a77581b
YMingF <834319313@qq.com>
2023-03-29 20:49:07 +0800

    9-1-个人中心页面初始化

---
+++ src/app/pages/member/member.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core'
+
+import { MemberRoutingModule } from './member-routing.module'
+import { CenterComponent } from './center/center.component'
+import { ShareModule } from '../../share/share.module'
+
+@NgModule({
+  declarations: [CenterComponent],
+  imports: [ShareModule, MemberRoutingModule],
+})
+export class MemberModule {}
